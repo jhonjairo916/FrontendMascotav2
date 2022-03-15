7b5a653177d8f05e35a9b74c8e906394f10358f1
jhonjairo916 <jhonjairo.916@hotmail.com>
2022-03-15 12:13:01 -0500

    Creation of modules

---
+++ src/app/modulos/mascota/mascota.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MascotaRoutingModule } from './mascota-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MascotaRoutingModule
+  ]
+})
+export class MascotaModule { }
---
+++ src/app/modulos/solicitud-mascota/solicitud-mascota.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SolicitudMascotaRoutingModule } from './solicitud-mascota-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SolicitudMascotaRoutingModule
+  ]
+})
+export class SolicitudMascotaModule { }
